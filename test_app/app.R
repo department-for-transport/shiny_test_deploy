0bcba3c7ac5e8c6e9c41436e44c90eb127aaae69
Fran <francesca.bryden@dft.gov.uk>
2022-07-12 14:18:27 +0100

    Change appearance of app

--- test_app/app.R
+++ test_app/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Deploying a change"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
